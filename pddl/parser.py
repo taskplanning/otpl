4685e998884bd60a82450494fed11db08a0c46b3
michael <michael.cashmore@strath.ac.uk>
2022-01-31 15:46:46 +0000

    Added some comments and hook for verbose parsing.

--- pddl/parser.py
+++ pddl/parser.py
@@ -1,9 +1,12 @@
 import sys
 import argparse
+from typing import Tuple
 from antlr4 import CommonTokenStream, FileStream
+from pddl.domain import Domain
 from pddl.grammar.pddl22Lexer import pddl22Lexer
 from pddl.grammar.pddl22Parser import pddl22Parser
 from pddl.parse_visitor import Parser
+from pddl.problem import Problem
 
 if __name__ == "__main__":  
     
@@ -11,28 +14,17 @@ if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("pddl_file", help="PDDL domain file, problem file, or both (max 2 files)", nargs='+')
     arg_parser.add_argument("-p","--print", action="store_true", help="Immediately print parsed PDDL.")
+    arg_parser.add_argument("-v","--verbose", action="store_true", help="Verbose parsing (not yet implemented).")
     args = arg_parser.parse_args()
 
     if len(args.pddl_file) > 2: 
         arg_parser.print_help()
         sys.exit(0)
-
+    
     for file in args.pddl_file:
-
-        # lexer
-        data = FileStream(file)
-        lexer = pddl22Lexer(data)
-        stream = CommonTokenStream(lexer)
         
-        # parser
-        arg_parser = pddl22Parser(stream)
-        tree = arg_parser.pddl_file()
-        if arg_parser.getNumberOfSyntaxErrors() > 0:
-            print("PDDL Parser encountered syntax errors.")
-            sys.exit(-1)
-
-        # visitor
-        visitor = Parser()
-        visitor.visit(tree)
-        if tree.domain() and args.print: print(visitor.domain)
-        if tree.problem() and args.print: print(visitor.problem)
+        pddl_parser = Parser()
+        pddl_parser.parse_file(file)
+        
+        if pddl_parser.domain and args.print: print(pddl_parser.domain)
+        if pddl_parser.problem and args.print: print(pddl_parser.problem)
