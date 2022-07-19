4e266345af58ae77cc62e5851e75fa35a1872447
Michael Cashmore <michael.cashmore@kcl.ac.uk>
2022-07-19 11:39:56 +0100

    Tidied up the relaxed plan example

--- examples/build_relaxed_plan_graph.py
+++ examples/build_relaxed_plan_graph.py
@@ -29,24 +29,15 @@ if __name__ == "__main__":
         pddl_parser.parse_file(file)
     print("Parsing PDDL files took %.2f seconds." % (time.time() - start_time))
 
-    # after parsing, prepare a grounded representation
-    print("Preparing grounded representation...")
-    start_time = time.time()
-    grounding = Grounding()
-    grounding.ground_problem(pddl_parser.domain, pddl_parser.problem)
-    print("Preparing grounded representation took %.2f seconds." % (time.time() - start_time))
-        
     # build a relaxed plan graph
     print("Building relaxed plan graph...")
     start_time = time.time()
-    rpg = RelaxedPlanGraph(pddl_parser.domain, pddl_parser.problem, grounding)
+    rpg = RelaxedPlanGraph(pddl_parser.domain, pddl_parser.problem)
     layer_count = rpg.build_graph(stop_at_goal=True)
     print("Building relaxed plan graph took %.2f seconds." % (time.time() - start_time))
     print("Relaxed plan graph has %d layers." % layer_count)
 
     # extract and print the relaxed plan
-    print("Relaxed Plan:")
-    plan, layer_count = rpg.get_relaxed_plan()
-    for layer, actions in plan.items():
-        for action_id in actions:
-            print("%d: %s" % (layer, grounding.get_action_from_id(action_id).print_pddl()))
+    relaxed_plan, action_count = rpg.get_relaxed_plan()
+    print("Relaxed plan has %d actions." % action_count)
+    rpg.print_relaxed_plan()
